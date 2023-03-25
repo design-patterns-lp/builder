98ac8130b02b9b0bdc1fe85eea35562210103ac9
Gabriel Faria <gabriel.selu@gmail.com>
2023-03-25 11:53:38 -0300

    feat: adiciona testes na classe computadorBuilderTest

--- src/test/java/builder/ComputadorBuilderTest.java
+++ src/test/java/builder/ComputadorBuilderTest.java
@@ -1,7 +1,6 @@
 package builder;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class ComputadorBuilderTest {
@@ -13,7 +12,7 @@ class ComputadorBuilderTest {
             Computador computador = computadorBuilder
                     .setModelo("Aspire 5")
                     .setProcessador("i5-8256U")
-                    .setArmazenamentoInternoMb(1)
+                    .setArmazenamentoInternoMb(1024)
                     .build();
             fail();
         } catch (IllegalArgumentException e) {
@@ -21,4 +20,62 @@ class ComputadorBuilderTest {
         }
     }
 
+    @Test
+    void deveRetornarExcecaoParaComputadorSemModelo() {
+        try {
+            ComputadorBuilder computadorBuilder = new ComputadorBuilder();
+            Computador computador = computadorBuilder
+                    .setMarca("Acer")
+                    .setProcessador("i5-8256U")
+                    .setArmazenamentoInternoMb(1024)
+                    .build();
+            fail();
+        } catch (IllegalArgumentException e) {
+            assertEquals("Modelo inválido", e.getMessage());
+        }
+    }
+
+    @Test
+    void deveRetornarExcecaoParaComputadorSemProcessador() {
+        try {
+            ComputadorBuilder computadorBuilder = new ComputadorBuilder();
+            Computador computador = computadorBuilder
+                    .setMarca("Acer")
+                    .setModelo("Aspire 5")
+                    .setArmazenamentoInternoMb(1024)
+                    .build();
+            fail();
+        } catch (IllegalArgumentException e) {
+            assertEquals("Processador inválido", e.getMessage());
+        }
+    }
+
+    @Test
+    void deveRetornarExcecaoParaComputadorSemArmazenamentoInternoMb() {
+        try {
+            ComputadorBuilder computadorBuilder = new ComputadorBuilder();
+            Computador computador = computadorBuilder
+                    .setMarca("Acer")
+                    .setModelo("Aspire 5")
+                    .setProcessador("i5-8256U")
+                    .build();
+            fail();
+        } catch (IllegalArgumentException e) {
+            assertEquals("Armazenamento inválido", e.getMessage());
+        }
+    }
+
+    @Test
+    void deveRetornarComputadorValido() {
+        ComputadorBuilder computadorBuilder = new ComputadorBuilder();
+        Computador computador = computadorBuilder
+                .setMarca("Acer")
+                .setModelo("Aspire 5")
+                .setProcessador("i5-8256U")
+                .setArmazenamentoInternoMb(1024)
+                .build();
+
+        assertNotNull(computador);
+
+    }
 }
